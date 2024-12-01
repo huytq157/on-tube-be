92bca69c3f87432df9f0e7a2dd35cd22f6845f92
huyNd1507 <huytran.dev157@gmail.com>
2024-12-01 19:40:30 +0700

    fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -107,7 +107,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 12 * 60 * 60 * 1000,
     });
 
